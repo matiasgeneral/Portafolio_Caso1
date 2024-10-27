cb261d97af31156f5ad2d5c6a7ba2bea4be4f6b2
DemWolf <ale.villa@duocuc.cl>
2024-10-26 22:03:48 -0300

    actualizar-ver-proyectos

--- Avance/src/app/admin/buscador-proyectos/buscador-proyectos.component.ts
+++ Avance/src/app/admin/buscador-proyectos/buscador-proyectos.component.ts
@@ -7,7 +7,7 @@ import { FirestoreService } from 'src/app/service/firestore.service';
   templateUrl: './buscador-proyectos.component.html',
   styleUrls: ['./buscador-proyectos.component.scss'],
 })
-export class BuscadorProyectosComponent  implements OnInit {
+export class BuscadorProyectosComponent implements OnInit {
   selectedDate: string = '';
   proyectos: any[] = [];
 
@@ -17,7 +17,10 @@ export class BuscadorProyectosComponent  implements OnInit {
     this.firestore.getdocs<any>('proyectos').subscribe((data: any[]) => {
       console.log('Datos recibidos de Firestore:', data);
       this.proyectos = data.map(proyecto => {
-        console.log('Noticia:', proyecto);
+        // Convertir las fechas de formato 'YYYY-MM-DD' a 'DD/MM/YYYY'
+        proyecto.fechaInicio = this.formatDateToDDMMYYYY(proyecto.fechaInicio);
+        proyecto.fechaFin = this.formatDateToDDMMYYYY(proyecto.fechaFin);
+        proyecto.fechaCreacion = this.formatDateToDDMMYYYY(proyecto.fechaCreacion); // Asegúrate de que la fecha de creación también se convierte
         return proyecto;
       });
       console.log('Proyectos después de map:', this.proyectos);
@@ -25,8 +28,15 @@ export class BuscadorProyectosComponent  implements OnInit {
   }
 
   administrarProyecto(proyecto: any) {
-    // Aquí puedes redirigir a la ruta correspondiente o abrir un modal
     this.router.navigate(['/administrar-proyectos', { id: proyecto.id }]);
   }
 
+  // Método para convertir fecha de 'YYYY-MM-DD' a 'DD/MM/YYYY'
+  private formatDateToDDMMYYYY(date: string): string {
+    const partes = date.split('-');
+    if (partes.length === 3) {
+      return `${partes[2]}/${partes[1]}/${partes[0]}`; // "DD/MM/YYYY"
+    }
+    return date; // Retornar la fecha original si no se puede convertir
+  }
 }
